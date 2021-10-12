73f9c3f213a7ecb299057e93dcf2a53679e62637
Gautom Agarwal <garggautam72@gmail.com>
2021-10-12 16:46:10 +0530

    delete bookmarks

--- app/src/main/java/com/example/shrutijagwani/browser/bookmarks.java
+++ app/src/main/java/com/example/shrutijagwani/browser/bookmarks.java
@@ -1,14 +1,20 @@
 package com.example.shrutijagwani.browser;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 import java.util.List;
@@ -17,31 +23,63 @@ public class bookmarks extends AppCompatActivity {
     myDbHandlerBook dbHandlerBook=new myDbHandlerBook(this,null,null,1);
     WebView mywebview;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookmarks);
 
-    final List<String> books=dbHandlerBook.databaseToString();
-    if(books.size()>0)
-    {
-        ArrayAdapter myadapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,books);
         ListView mylist=(ListView)findViewById(R.id.listViewBook);
-        mylist.setAdapter(myadapter);
+        final List<String> books=dbHandlerBook.databaseToString();
+        final ArrayAdapter myadapter = new ArrayAdapter<String>(this,R.layout.custom_view,R.id.txt
+                ,books);
 
-        mylist.setOnItemClickListener(
-                new AdapterView.OnItemClickListener(){
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        String url=books.get(position);
-                        Intent intent = new Intent(bookmarks.this,MainActivity.class);
-                        intent.putExtra("url",url);
-                        startActivity(intent);
-                        finish();
+
+
+        if(books.size()>0)
+        {
+//        ArrayAdapter myadapter = new ArrayAdapter<String>(this,R.layout.custom_view,books);
+
+            mylist.setAdapter(myadapter);
+
+            mylist.setOnItemClickListener(
+                    new AdapterView.OnItemClickListener(){
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            String url=books.get(position);
+                            Intent intent = new Intent(bookmarks.this,MainActivity.class);
+                            intent.putExtra("url",url);
+                            startActivity(intent);
+                            finish();
+                        }
                     }
-                }
-        );
-    }
+            );
+        }
+
+        mylist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final int position = i;
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(bookmarks.this);
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dbHandlerBook.deleteUrl(books.get(position).toString());
+                        books.remove(position);
+                        myadapter.notifyDataSetChanged();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+                builder.setMessage("Do you want to delete this bookmark?");
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
     }
 }
